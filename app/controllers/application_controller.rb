654051b75dad48d9ff35675ae48821404a7bbd5a
GeoReece000 <geofreytegeret@gmail.com>
2023-06-10 11:21:19 +0300

    postart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,3 +7,15 @@ class ApplicationController < Sinatra::Base
     articles = Article.all
     articles.to_json
   end
+
+  post '/articles' do
+    article = Article.new(article_params)
+
+    if article.save
+      status 201
+      article.to_json
+    else
+      status 422
+      { error: 'Unable to create article' }.to_json
+    end
+  end
