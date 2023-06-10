b7a81722ac6d16276931351e635b7700e52f9552
GeoReece000 <geofreytegeret@gmail.com>
2023-06-10 11:17:47 +0300

    faker1stcommit

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,37 +8,3 @@ puts "🌱 Seeding spices..."
 Article.destroy_all
 Author.destroy_all
 Category.destroy_all
-
-# Generate fake authors
-10.times do
-  Author.create(
-    author_name: Faker::Name.name,
-    writing_category: Faker::Book.genre
-  )
-end
-
-# Generate fake categories
-5.times do
-  Category.create(
-    category_name: Faker::Book.genre
-  )
-end
-
-# Generate fake articles
-30.times do
-  author = Author.all.sample
-  category = Category.all.sample
-
-  Article.create(
-    author_name: author.author_name,
-    views: Faker::Number.between(from: 0, to: 1000),
-    author_id: author.id,
-    category_id: category.id,
-    # created_at: Faker::Time.between(from: 1.year.ago, to: Time.zone),
-    # updated_at: Faker::Time.between(from: 1.year.ago, to: Time.zone),
-    comments: Faker::Lorem.paragraph,
-    article: Faker::Lorem.paragraphs(number: 3).join("\n\n")
-  )
-end
-
-puts "✅ Done seeding!"
