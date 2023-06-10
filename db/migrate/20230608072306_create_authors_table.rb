bb6201e85a7119ba432204e25d4ed15da91592db
GeoReece000 <geofreytegeret@gmail.com>
2023-06-10 11:14:10 +0300

    authortable

--- db/migrate/20230608072306_create_authors_table.rb
+++ db/migrate/20230608072306_create_authors_table.rb
@@ -1,2 +1,8 @@
 class CreateAuthorsTable < ActiveRecord::Migration[5.2]
   def change
+    create_table :authors do |table|
+      table.string :author_name
+      table.string :writing_category
+    end
+  end
+end
