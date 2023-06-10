c45d65cae3a4bc20044d251bf39b671d715f48b0
GeoReece000 <geofreytegeret@gmail.com>
2023-06-10 11:15:04 +0300

    commenttable

--- db/migrate/20230608073347_create_comment_table.rb
+++ db/migrate/20230608073347_create_comment_table.rb
@@ -1,2 +1,10 @@
 class CreateCommentTable < ActiveRecord::Migration[5.2]
   def change
+    create_table :comments do |table|
+      table.string :user_name
+      table.string :comments
+      table.integer :article_id
+      table.timestamps
+    end
+  end
+end
