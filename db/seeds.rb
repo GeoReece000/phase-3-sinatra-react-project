eced58927f0f6bff124f9792ad1bb8f595f7d24a
GeoReece000 <geofreytegeret@gmail.com>
2023-06-10 11:19:06 +0300

    fakearticles

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,3 +23,22 @@ end
     category_name: Faker::Book.genre
   )
 end
+
+# Generate fake articles
+30.times do
+  author = Author.all.sample
+  category = Category.all.sample
+
+  Article.create(
+    author_name: author.author_name,
+    views: Faker::Number.between(from: 0, to: 1000),
+    author_id: author.id,
+    category_id: category.id,
+    # created_at: Faker::Time.between(from: 1.year.ago, to: Time.zone),
+    # updated_at: Faker::Time.between(from: 1.year.ago, to: Time.zone),
+    comments: Faker::Lorem.paragraph,
+    article: Faker::Lorem.paragraphs(number: 3).join("\n\n")
+  )
+end
+
+puts "✅ Done seeding!"
